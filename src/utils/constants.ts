66c8fccbdae570baee7ff09d50735c7702749227
Alis Akers <alismxwll@gmail.com>
2025-05-30 10:25:56 -0700

    chore: update endpoint URLs in constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
 export const PAGES = {
   LANDING: "/",
   // demo urls
-  ECR_VIEWER: "#",
-  ECR_REFINER: "#",
-  QUERY_CONNECTOR: "https://demo.queryconnector.dev",
+  ECR_VIEWER: "https://viewer.dibbs.tools",
+  ECR_REFINER: "https://refiner.dibbs.tools",
+  QUERY_CONNECTOR: "https://connector.dibbs.tools",
   SIMPLE_REPORT: "https://demo.simplereport.gov",
-  RECORD_LINKER: "https://recordlinker.dibbs.demo",
+  RECORD_LINKER: "https://linker.dibbs.tools",
   // repo urls
   QUERY_CONNECTOR_REPO: "https://github.com/CDCgov/dibbs-query-connector",
   RECORD_LINKER_REPO: "https://github.com/CDCgov/RecordLinker",
