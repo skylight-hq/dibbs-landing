4b871c186adcc26a7ea696476af54dccff21bd44
johanna-skylight <johanna@skylight.digital>
2025-06-03 08:57:19 -0400

    Updated simple report url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export const PAGES = {
   ECR_VIEWER: "#",
   ECR_REFINER: "#",
   QUERY_CONNECTOR: "https://demo.queryconnector.dev",
-  SIMPLE_REPORT: "https://demo.simplereport.gov",
+  SIMPLE_REPORT: "https://demo.simplereport.gov/app/results/universal?facility=a8740505-f8c0-47e4-bd1b-e21d5dea3b4a",
   RECORD_LINKER: "https://recordlinker.dibbs.demo",
   // repo urls
   QUERY_CONNECTOR_REPO: "https://github.com/CDCgov/dibbs-query-connector",
